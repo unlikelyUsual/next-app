616b51f4d4b72c0e090860cc9f65d982f9a240f5
Prabhat <prabhat@Prabhats-Macbook.local>
2024-04-22 22:00:25 +0530

    middleware file

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,25 +1,44 @@
 import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { UserRole } from "./util/enum";
 
-const publicRoute = ["/login", "/signup"];
+// 1. Describing routes
+const publicRoutes = ["/login", "/signup"];
 const privateRoutes = ["/profile"];
 const adminRoutes = ["/admin"];
 
-export async function middleware(request: NextRequest) {
-  const cookie = request.cookies.get("token")?.value ?? "";
+export default async function middleware(req: NextRequest) {
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = privateRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+  const isAdminRoute = adminRoutes.includes(path);
 
-  const decoded: any = jwt.verify(cookie, process.env.JWT_KEY!);
+  // 3. Decrypt the session from the cookie
+  const token = cookies().get("token")?.value as string;
+  const session: any = jwt.verify(token, process.env.JWT_KEY!);
 
-  const role = decoded.role as UserRole;
+  // 5. Redirect to /login if the user is not authenticated
+  if ((isProtectedRoute || isAdminRoute) && !session?.id) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
-  request.headers.append("uid", decoded.id);
+  // 6. Redirect to /dashboard if the user is not admin
+  if (isAdminRoute && session.role !== UserRole.ADMIN) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
 
-  return NextResponse.redirect(new URL("/home", request.url));
+  // 6. Redirect to /dashboard if the user is authenticated
+  if (isPublicRoute && session?.id && !path.startsWith("/profile")) {
+    return NextResponse.redirect(new URL("/profile", req.nextUrl));
+  }
+
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+// Routes Middleware should not run on
 export const config = {
-  matcher: ["/*"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
